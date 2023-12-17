35af3400009ff61c94effae2024db157866d0e66
SafwanBackar <98946926+SafwanBackar@users.noreply.github.com>
2023-12-17 12:42:11 +0530

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Many2many Widget Clickable",
-    "author": "https://github.com/SafwanBackar/",
+    "author": "",
     'description': """This module extends the many2many widget to make it clickable and view the record. In your xml file where many2many field exists, add widget as 'many2many_taglinks'.
                 You can add effects like mouse hover to highlight the many2many tag and other addons.!!
                 """,
